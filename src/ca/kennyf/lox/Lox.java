7d2fdb7a2ba2d119ac08a701fa0d1e2482046484
Ken Fowler <developmetn@kennyf.ca>
2022-08-01 18:18:34 -0400

    print out tokens emitted by scanner

--- src/ca/kennyf/lox/Lox.java
+++ src/ca/kennyf/lox/Lox.java
@@ -1,3 +1,5 @@
+package ca.kennyf.lox;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -5,7 +7,6 @@ import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.List;
-import java.util.Scanner;
 
 public class Lox {
     static boolean hadError = false;
@@ -42,14 +43,20 @@ public class Lox {
     private static void run(String source) {
         Scanner scanner = new Scanner(source);
         List<Token> tokens = scanner.scanTokens();
-    }
-    private static void report(int line, String where, String message) {
-        System.err.println("[line " + line + "] Error " + where + ": " + message);
-        hadError = true;
+
+        for (Token token: tokens) {
+            System.out.println(token);
+        }
     }
 
     static void error(int line, String message) {
         report(line, "", message);
     }
+
+    private static void report(int line, String where, String message) {
+        System.err.println("[line " + line + "] Error " + where + ": " + message);
+        hadError = true;
+    }
+
 
 }
