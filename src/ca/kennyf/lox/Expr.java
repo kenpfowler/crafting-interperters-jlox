2070af94199b89e2377a3d1ca8138c1e2fe0091d
Ken Fowler <developmetn@kennyf.ca>
2022-08-02 22:26:38 -0400

    added classes for building ast

--- src/ca/kennyf/lox/Expr.java
+++ src/ca/kennyf/lox/Expr.java
@@ -1,8 +1,12 @@
 package ca.kennyf.lox;
 
-import java.util.List;
-
 abstract class Expr {
+    interface Visitor<R> {
+    R visitBinaryExpr(Binary expr);
+    R visitGroupingExpr(Grouping expr);
+    R visitLiteralExpr(Literal expr);
+    R visitUnaryExpr(Unary expr);
+    }
  static class Binary extends Expr {
     Binary(Expr left, Token operator, Expr right) {
     this.left = left;
@@ -10,6 +14,10 @@ abstract class Expr {
     this.right = right;
     }
 
+    @Override
+    <R> R accept(Visitor<R> visitor) {
+    return visitor.visitBinaryExpr(this);
+    }
     final Expr left;
     final Token operator;
     final Expr right;
@@ -19,6 +27,10 @@ abstract class Expr {
     this.expression = expression;
     }
 
+    @Override
+    <R> R accept(Visitor<R> visitor) {
+    return visitor.visitGroupingExpr(this);
+    }
     final Expr expression;
     }
  static class Literal extends Expr {
@@ -26,6 +38,10 @@ abstract class Expr {
     this.value = value;
     }
 
+    @Override
+    <R> R accept(Visitor<R> visitor) {
+    return visitor.visitLiteralExpr(this);
+    }
     final Object value;
     }
  static class Unary extends Expr {
@@ -34,7 +50,13 @@ abstract class Expr {
     this.right = right;
     }
 
+    @Override
+    <R> R accept(Visitor<R> visitor) {
+    return visitor.visitUnaryExpr(this);
+    }
     final Token operator;
     final Expr right;
     }
+
+    abstract <R> R accept(Visitor<R> visitor);
 }
